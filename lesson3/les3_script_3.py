3209d2d66984aec26f74a86c7b5b9ff36538bab6
davidodav <dawita3@gmail.com>
2019-12-23 22:46:43 -0500

    best version of the script

--- lesson3/les3_script_3.py
+++ lesson3/les3_script_3.py
@@ -8,25 +8,29 @@ loop once you have retrieved these two items.
 '''
 from __future__ import print_function, unicode_literals
 
-with open('show_lldp_neighbors_detail.txt') as f:
+with open("show_lldp_neighbors_detail.txt") as f:
     show_lldp = f.read()
-
+    
 (res1, res2) = (False, False)
+
 for line in show_lldp.splitlines():
     if line.startswith('----'):
         continue
     elif "System Name" in line:
         fields = line.split(':')
         sys_name = fields[1]
-        res1 = True
+        res1  = True
     elif "Port id" in line:
         fields = line.split(':')
         port_id = fields[1]
         res2 = True
-    print(f"System Name and Port ID are : {sys_name} and {port_id}")
+    
     if res1 and res2:
-        print("Exiting...")
         break
 print()
+print(f"System Name: {sys_name}")
+print(f"Port ID: {port_id}")
+print()
+
     
     
